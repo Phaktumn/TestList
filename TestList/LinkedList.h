53864b5430fb98a0dda536d9826dd160cbd22f7c
Jorge Paiva <fskjorge@gmail.com>
2016-04-13 02:28:07 +0100

    FIRST

---
+++ TestList/LinkedList.h
@@ -0,0 +1,186 @@
+#define LINKED_LIST_H
+#ifdef LINKED_LIST_H
+
+#include "stdafx.h"
+#include <cstdlib>
+#pragma once
+
+#define  TRUE  1
+#define  FALSE 0
+
+template<class T> class Simple_Array
+{
+	typedef struct Node {
+		T _Item;
+		Node* _Next;
+	};
+
+	Node* _Front_Ptr;
+	int _size;
+public:
+
+	void push_back(T _value)
+	{
+		if (_Front_Ptr == nullptr) {
+			_Front_Ptr = (Node*)(malloc(sizeof(Node)));
+			_Front_Ptr->_Item = _value;
+			_Front_Ptr->_Next = nullptr;
+			_size++;
+		}
+		else {
+			Node* _Prev_Ptr = _Front_Ptr;
+			Node* _Ptr = _Front_Ptr;
+
+			while (_Ptr != nullptr)
+			{
+				_Prev_Ptr = _Ptr;
+				_Ptr = _Ptr->_Next;
+			}
+
+			_Ptr = (Node*)malloc(sizeof(Node));
+			_Prev_Ptr->_Next = _Ptr;
+			_Ptr->_Item = _value;
+			_Ptr->_Next = nullptr;
+			_size++;
+		}
+	}
+
+	void pop_Back()
+	{
+		if (_Front_Ptr == nullptr) return;
+		Node* _Ptr = _Front_Ptr;
+		Node* _Prev_Ptr = _Front_Ptr;
+		while (_Ptr != nullptr) {
+			_Prev_Ptr = _Ptr;
+			_Ptr = _Ptr->_Next;
+			if (_Ptr->_Next == nullptr) {
+				_Prev_Ptr->_Next = nullptr;
+				free(_Ptr);
+				_size--;
+				break;
+			}
+		}
+	}
+
+	T operator [](int _Index)
+	{
+		int _curr_Index = 0;
+		Node* _Ptr = _Front_Ptr;
+		if (_Ptr != nullptr) {
+			while (_curr_Index < _Index) {
+				_Ptr = _Ptr->_Next;
+				if (_Ptr == nullptr) return 0;
+				_curr_Index++;
+			}
+			return _Ptr->_Item;
+		}
+		return 0;
+	}
+
+	void addAt(unsigned int _Index, T _data)
+	{
+		int _curr_Index = 1;
+		Node* _Ptr = _Front_Ptr;
+		Node* _last_Ptr = _Front_Ptr;
+		if(_Index == 1){
+			_Front_Ptr->_Next = _last_Ptr;
+			_Front_Ptr->_Item = _data;
+		}
+		else {
+			Node* _New_Data_;
+			while (_Ptr != nullptr) {
+				_curr_Index++;
+				_last_Ptr = _Ptr;
+				_Ptr = _Ptr->_Next;
+				if (_curr_Index == _Index)
+					break;
+			}
+			_New_Data_ = (Node*)(malloc(sizeof(Node)));
+			_New_Data_->_Item = _data;
+			_New_Data_->_Next = _Ptr;
+			_last_Ptr->_Next = _New_Data_;	
+		}
+		_size++;
+	}
+
+	void removeAt(unsigned int _Index)
+	{
+		unsigned short int _curr_Index = 1;
+		Node* _Ptr = _Front_Ptr;
+		//Remove The first Node
+		if (_Index == 1) {
+			_Front_Ptr = _Ptr->_Next;
+			free(_Ptr);
+			_size--;
+		}
+
+		else {
+			Node* _last_Ptr = _Front_Ptr;
+			while (_Ptr != nullptr) {
+				_curr_Index++;
+				_last_Ptr = _Ptr;
+				_Ptr = _Ptr->_Next;
+				if (_curr_Index == _Index) {
+					_size--;
+					break;
+				}
+			}
+			_last_Ptr->_Next = _Ptr->_Next;
+			free(_Ptr);
+		}
+	}
+
+	void Clear()
+	{
+		Node* _Ptr = _Front_Ptr;
+		Node* _aux__Ptr = _Front_Ptr;
+		while (_Ptr != nullptr) {
+			_Ptr = _aux__Ptr;
+			if (_Ptr->_Next == nullptr){
+				free(_aux__Ptr);
+				_size--;
+				break;
+			}
+			_aux__Ptr = _Ptr->_Next;
+			free(_Ptr);
+			_size--;
+		}
+	}
+
+	T front()
+	{
+		if (_Front_Ptr == nullptr) return NULL;
+		return _Front_Ptr->_Item;
+	}
+
+	T back()
+	{
+		Node* _Ptr = _Front_Ptr;
+		while (_Ptr != nullptr) {
+			if (_Ptr->_Next == nullptr)
+				break;
+			_Ptr = _Ptr->_Next;
+		}
+		return _Ptr->_Item;
+	}
+
+	unsigned int isEmpty() const
+	{
+		if (size() == 0) return TRUE;
+		return FALSE;
+	}
+
+	int size() const {
+		return _size;
+	}
+
+	Simple_Array()
+		: _Front_Ptr{ nullptr },
+		_size{ 0 }
+	{ }
+
+	~Simple_Array()
+	{ }
+};
+
+#endif //LINKED_LIST_H
---
+++ TestList/TestList.cpp
@@ -0,0 +1,85 @@
+// TestList.cpp : Defines the entry point for the console application.\
+// Open ReadMe.txt
+//
+
+#include "stdafx.h"
+
+class Simple
+{
+public:
+	explicit Simple(int m);
+	int m;
+};
+
+Simple::Simple(int m)
+{
+	this->m = m;
+}
+
+void print(Simple_Array<int> test)
+{
+	for (size_t i = 0; i < test.size(); i++) {
+		std::cout << test[i] << "|";
+	}
+	std::cout << "\n";
+}
+
+void print(Simple_Array<Simple*> test)
+{
+	for (size_t i = 0; i < test.size(); i++) {
+		std::cout << test[i]->m << "|";
+	}
+	std::cout << "\n";
+}
+
+int main()
+{
+	Simple_Array<int> test;
+	Simple_Array<Simple*> simpleA;
+
+	simpleA.push_back(new Simple(2));
+	simpleA.push_back(new Simple(4));
+	simpleA.push_back(new Simple(5));
+	simpleA.push_back(new Simple(12));
+	simpleA.push_back(new Simple(16));
+
+	std::cout << "Object List Initial State" << std::endl;
+	print(simpleA);
+
+	std::cout << "Remove Node 2" << std::endl;
+	simpleA.removeAt(2);
+	print(simpleA);
+
+	std::cout << "Add new Object at Position 1 " << std::endl;
+	simpleA.addAt(1, new Simple(69));
+	print(simpleA);
+
+	test.push_back(2);  //1
+	test.push_back(5);  //2
+	test.push_back(15); //3
+	test.push_back(12); //4
+	test.push_back(16); //5
+	test.push_back(123);//6
+
+	//test.Clear();
+
+	std::cout << "INT List Initial State" << std::endl;
+	print(test);
+
+	std::cout << "pop last node" << std::endl;
+	test.pop_Back();
+	print(test);
+
+	std::cout << "remove node number 3" << std::endl;
+	test.removeAt(3);
+	print(test);
+
+	std::cout << "\n";
+	std::cout << "Front of the List: " << test.front() << std::endl;
+	std::cout << "Back of the list: " << test.back() << std::endl;
+
+	system("pause");
+
+    return 0;
+}
+
